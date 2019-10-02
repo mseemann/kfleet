c3acf8b54888ef0696505ff4ce777ea405100b27
Michael Seemann <michael.seemann@synaos.com>
2019-10-02 12:15:37 +0200

    improve test coverage

--- cars-service/src/main/kotlin/io/kfleet/cars/service/repos/CommandsResponseRepository.kt
+++ cars-service/src/main/kotlin/io/kfleet/cars/service/repos/CommandsResponseRepository.kt
@@ -1,19 +1,21 @@
 package io.kfleet.cars.service.repos
 
+import io.kfleet.cars.service.WebClientUtil
 import io.kfleet.cars.service.processors.OwnerCommandsProcessorBinding
 import io.kfleet.cars.service.rpclayer.RPC_COMMAND_RESPONSE
 import io.kfleet.commands.CommandResponse
-import io.kfleet.common.createWebClient
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 
 @Repository
-class CommandsResponseRepository(private val interactiveQueryService: InteractiveQueryService) {
+class CommandsResponseRepository(
+        private val interactiveQueryService: InteractiveQueryService,
+        private val webClientUtil: WebClientUtil) {
 
     fun findCommandResponse(commandId: String): Mono<CommandResponse> {
         val hostInfo = interactiveQueryService.getHostInfo(OwnerCommandsProcessorBinding.OWNER_COMMANDS_RESPONSE_STORE, commandId, StringSerializer())
-        return createWebClient(hostInfo).get().uri("$RPC_COMMAND_RESPONSE/$commandId").retrieve().bodyToMono(CommandResponse::class.java)
+        return webClientUtil.doGet(hostInfo, "$RPC_COMMAND_RESPONSE/$commandId", CommandResponse::class.java)
     }
 }
