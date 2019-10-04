b76007c899a092c154814e1ffffdd707480e9d5a
Michael Seemann <michael.seemann@synaos.com>
2019-10-04 17:24:02 +0200

    3 broker; exactyl once

--- cars-service/src/test/kotlin/io/kfleet/cars/service/processors/ProcessorIntegrationTests.kt
+++ cars-service/src/test/kotlin/io/kfleet/cars/service/processors/ProcessorIntegrationTests.kt
@@ -14,6 +14,7 @@ import org.awaitility.kotlin.await
 import org.awaitility.kotlin.untilAsserted
 import org.awaitility.kotlin.withPollInterval
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
