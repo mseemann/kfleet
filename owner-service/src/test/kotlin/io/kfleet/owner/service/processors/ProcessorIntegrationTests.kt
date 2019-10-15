87783a2e7b2e0b12f5f49a020551b34496addac4
Michael Seemann <michael.seemann@synaos.com>
2019-10-15 21:54:43 +0200

    improve test coverage

--- owner-service/src/test/kotlin/io/kfleet/owner/service/processors/ProcessorIntegrationTests.kt
+++ owner-service/src/test/kotlin/io/kfleet/owner/service/processors/ProcessorIntegrationTests.kt
@@ -7,6 +7,7 @@ import io.kfleet.owner.service.domain.CarModel
 import io.kfleet.owner.service.repos.*
 import io.kfleet.owner.service.web.NewCar
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -15,7 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 import testing.KafkaContextInitializer
 import kotlin.test.*
 
-//@EnabledIfEnvironmentVariable(named = "ENV", matches = "ci")
+@EnabledIfEnvironmentVariable(named = "ENV", matches = "ci")
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(initializers = [KafkaContextInitializer::class])
