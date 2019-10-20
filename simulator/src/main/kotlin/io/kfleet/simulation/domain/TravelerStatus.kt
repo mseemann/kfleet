de6de5bd1d472aee2029c7c5970fa3dafcddb00d
Michael Seemann <michael.seemann@synaos.com>
2019-10-20 16:35:30 +0200

    remove unused code

--- simulator/src/main/kotlin/io/kfleet/simulation/domain/TravelerStatus.kt
+++
@@ -1,7 +0,0 @@
-package io.kfleet.simulation.domain
-
-enum class TravelerStatus {
-    IS_GOING, // sits in a car and is going to its destination
-    IS_WAITING, // waits for a car to pic him up
-    IS_LIVING // dose nothing special
-}
