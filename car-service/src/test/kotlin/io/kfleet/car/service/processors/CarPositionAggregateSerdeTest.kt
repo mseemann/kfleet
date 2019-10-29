725c30d5663be245a8b803fc8d53c159d77baa78
Michael Seemann <michael.seemann@synaos.com>
2019-10-29 20:00:05 +0100

    test coverage

--- car-service/src/test/kotlin/io/kfleet/car/service/processors/CarPositionAggregateSerdeTest.kt
+++ car-service/src/test/kotlin/io/kfleet/car/service/processors/CarPositionAggregateSerdeTest.kt
@@ -3,6 +3,7 @@ package io.kfleet.car.service.processors
 import io.kfleet.car.service.processor.CarPositionAggregate
 import io.kfleet.car.service.processor.CarPositionAggregateSerde
 import org.junit.jupiter.api.Test
+import kotlin.test.assertNull
 import kotlin.test.expect
 
 class CarPositionAggregateSerdeTest {
@@ -20,5 +21,20 @@ class CarPositionAggregateSerdeTest {
         val deserialized = serde.deserializer().deserialize("test", serialized)
 
         expect(agg.quadrants) { deserialized.quadrants }
+    }
+
+    @Test
+    fun deserializeCarPositionAggregate() {
+
+        val agg = CarPositionAggregate()
+        agg.add("1", "c1")
+
+        val serde = CarPositionAggregateSerde()
+
+        val deserializedNull = serde.deserializer().deserialize("test", null)
+        assertNull(deserializedNull)
+
+        val deserializedEmpty = serde.deserializer().deserialize("test", byteArrayOf())
+        assertNull(deserializedEmpty)
     }
 }
