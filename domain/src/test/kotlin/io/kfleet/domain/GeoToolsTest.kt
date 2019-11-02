06a7b8a97e24694c5947433c8938884fa1940689
Michael Seemann <michael.seemann@synaos.com>
2019-11-02 11:16:12 +0100

    complete geo tools tests

--- domain/src/test/kotlin/io/kfleet/domain/GeoToolsTest.kt
+++ domain/src/test/kotlin/io/kfleet/domain/GeoToolsTest.kt
@@ -18,6 +18,9 @@ class GeoToolsTest {
         val boxes = QuadTree.boundingBoxes(quadrantAndNodes);
         boxes.forEach { println(it) }
 
+        val firstNode = quadrantAndNodes.get(0).node
+        expect(Node(0.0, 0.0, w = 180.0, h = 90.0).toString()) { firstNode.toString() }
+
         val index = QuadTree.encodedIndexPath(lng = lng, lat = lat)
         expect("2/1/4/1/4/2/3/2/4/2/2") { index }
     }
@@ -61,5 +64,11 @@ class GeoToolsTest {
 
         val index = QuadTree.encodedIndexPath(lng = lng, lat = lat)
         expect("4/2/4/2/2/2/2/4/2/3/4") { index }
+    }
+
+    @Test
+    fun testIndexPathsForBoundingBoxRoot() {
+        val paths = QuadTree.getIndexesArround(lng = 0.0, lat = 0.0, withMinDistanceInKilometers = 0.0)
+        expect(0) { paths.size }
     }
 }
